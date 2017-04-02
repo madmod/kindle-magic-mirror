484d454ef54dd608d3c1aedf5c1e24be367d6c36
John Wells <madmodmail@gmail.com>
2017-04-02 13:45:52 -0600

    Increase maxDaysForecast to 7.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ window.config = {
     apiKey: '01a457e8decf58c6c551b6870a639d2d',
 		latitude: '40.4341',
 		longitude: '-111.7564',
-		maxDaysForecast: 5,
+		maxDaysForecast: 7,
 		// TODO: See if this works on the Kindle.
 		showPrecipitationGraph: false,
 		// Update every other minute. (Max free API calls per day is 1,000 so 720 is close.)
