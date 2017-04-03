156e7540d0f533284b683761804633229ca25225
John Wells <madmodmail@gmail.com>
2017-04-03 06:44:05 -0600

    Update DarkSky API Key.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.config = {
   language: 'en',
 
   'MMM-forecast-io': {
-    apiKey: '01a457e8decf58c6c551b6870a639d2d',
+    apiKey: '695c00bfa61f719cbe6e0a23821e2251',
 		latitude: '40.4341',
 		longitude: '-111.7564',
 		maxDaysForecast: 7,
@@ -13,5 +13,11 @@ window.config = {
 		showPrecipitationGraph: false,
 		// Update every other minute. (Max free API calls per day is 1,000 so 720 is close.)
 		updateInterval: 60 * 2 * 1000
+  }/*,
+
+  'MMM-plex': {
+		playerId: 'dd5b56780c1ef764df731e4f6c214946',
+		apiBase: 'http://server.local:1880/plex'
   }
+	*/
 };
